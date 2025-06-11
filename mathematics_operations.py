76bc383645a71e33cdc64ce10dfb9fc59b7f2ffa
Raakhirachel <rr21164@essex.ac.uk>
2025-06-11 08:49:31 +0530

    first commit

---
+++ mathematics_operations.py
@@ -0,0 +1,12 @@
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+
+Add = int(a) + int(b)
+Subtract = int(a) - int(b)
+Multiply = int(a) * int(b)
+Divide = int(a) / int(b)
+
+print("Addition:", Add)
+print("Subtraction:", Subtract)
+print("Multiplication:", Multiply)
+print("Division:", Divide)
